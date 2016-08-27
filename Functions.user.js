5846154e2ed5f91eb534bee11befa081d04836a4
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-08-27 05:39:45 +0300

    Update and rename Lib.JS.Functions.user.js to Functions.user.js

--- Functions.user.js
+++ Functions.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Lib.JS.Functions
+// @name         Functions.user.js
 // @version      1.0.0
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @grant        none
-// @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Lib.JS.Functions.user.js
+// @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Functions.user.js
 // ==/UserScript==
 
 // GLOBAL FUNCTIONS
