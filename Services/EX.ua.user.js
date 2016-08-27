e323a75cbac726122b8cc95e0821a1c6c701bbc0
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-08-27 07:30:03 +0300

    Add files via upload

--- Services/EX.ua.user.js
+++ Services/EX.ua.user.js
@@ -16,42 +16,55 @@
 (function() {
   'use strict';
 
-  var test = false;
-  // var test = true;
+  /* JS.AddEmbedCodeFrame.Lib.user.js GLOBAL VARIABLES
+  // ====================================================================================================================
+  var pageHost = location.hostname, pageURL = location.href, pageTitle = document.title;
+  var createLink = true, createPoster = true, contentURL, posterURL, appendToFrame, appendPosition;
+  var embedCodeFrame_Margin, embedCodeLink_Margin, embedCodeFrame_BackgroundColor;
+  var contentTitle;
+  var embedCodeText;
+  var posters = [];
+  var qualityButtons = [];
+  var textAreaAutoHeight = false, textAreaFixedHeight = false;
+  var embedCodeTextRefresh = true;
+  */
 
-  if (
-    pageURL.matchLink('http://www.ex.ua/*')
-  ) {
-    mainFunction = function() {
-      var tmpArray = [], playList = [];
-      createLink = false;
-      createPoster = false;
-      appendToFrame = document.querySelector('#body_element');
-      appendPosition = 'append';
+  // THIS FILE GLOBAL VARIABLES
+  // ====================================================================================================================
+  var mainFunction;
+  var delay = 1000,
+      tries = 15;
+  // ====================================================================================================================
 
-      var player_info_array = player_info;
-      forEach(player_info_array, function(index, self) {
-        // var position = self.pos;
-        var title = self.title;
-        title = title.replace(/(.*)\..*$/g, "$1");
-        tmpArray.push(title);
-      });
+  mainFunction = function() {
+    var tmpArray = [], playList = [];
+    createLink = false;
+    createPoster = false;
+    textAreaAutoHeight = true;
+    embedCodeTextRefresh = false;
+    appendToFrame = document.querySelector('#body_element');
+    appendPosition = 'append';
 
-      var player_list_array = JSON.parse('['+player_list+']');
-      forEach(player_list_array, function(index, self) {
-        var videoSrc = self.url;
-        var videoTitle = tmpArray[index];
-        var groupTitle = pageTitle.replace('@ EX.UA', '');
-        if (index < 1) embedCodeText = '#EXTM3U\n';
-        embedCodeText = embedCodeText + ('#EXTINF: -1 group-title="'+groupTitle+'",'+videoTitle+'\n'+videoSrc) + '\n';
-      });
+    var player_info_array = player_info;
+    forEach(player_info_array, function(index, self) {
+      // var position = self.pos;
+      var title = self.title;
+      title = title.replace(/(.*)\..*$/g, "$1");
+      tmpArray.push(title);
+    });
 
-      addEmbedCodeFrame();
-      auto_grow(document.getElementById("oldEmbedCodeFrame").querySelector('textarea'));
-      addKeyComboCtrlC(true);
-      clearTimeout();
-    };
-    mainFunction();
-    if (test) alert('test: 1');
-  }
+    var player_list_array = JSON.parse('['+player_list+']');
+    forEach(player_list_array, function(index, self) {
+      var videoSrc = self.url;
+      var videoTitle = tmpArray[index];
+      var groupTitle = pageTitle.replace('@ EX.UA', '');
+      if (index < 1) embedCodeText = '#EXTM3U\n';
+      embedCodeText = embedCodeText + ('#EXTINF: -1 group-title="'+groupTitle+'",'+videoTitle+'\n'+videoSrc) + '\n';
+    });
+
+    addEmbedCodeFrame(mainFunction);
+    addKeyComboCtrlC(true);
+  };
+
+  mainFunction();
 })();
