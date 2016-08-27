92e30fcdf7e00278962feded6ff32c332c8d10af
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-08-27 05:41:14 +0300

    Update Functions.user.js

--- Functions.user.js
+++ Functions.user.js
@@ -1,10 +1,3 @@
-// ==UserScript==
 // @name         Functions.user.js
-// @version      1.0.0
-// @description  Pure JavaScript version.
-// @author       Ægir
-// @grant        none
-// @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Functions.user.js
-// ==/UserScript==
 
 
