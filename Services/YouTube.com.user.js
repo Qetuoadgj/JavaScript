6833e754e474b40ddaa5e2e97de8638e997ca642
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-09-15 23:51:54 +0300

    Add files via upload

--- Services/YouTube.com.user.js
+++ Services/YouTube.com.user.js
@@ -37,6 +37,8 @@
   // ====================================================================================================================
 
   mainFunction = function() {
+    var canBeEmbed = document.querySelector('link[itemprop="embedURL"]');
+    if (!canBeEmbed) return;
     var flashvars = '?' +
         'start=0' +'&'+    // Start time
         'autoplay=1' +'&'+ // Enable Autoplay
