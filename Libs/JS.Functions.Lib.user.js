0e2aa9ee5323e0d52b098fcc2cf3c177b6d87c91
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-09-20 03:31:10 +0300

    Add files via upload

--- Libs/JS.Functions.Lib.user.js
+++ Libs/JS.Functions.Lib.user.js
@@ -135,7 +135,7 @@ function MouseWheelAudioControl(media, step) {
   var fontSize = 72;
 
   var volumeText = document.createElement('div');
-  volumeText.style.color = 'yellow';
+  volumeText.style.color = 'yellow !important';
   volumeText.style['font-size'] = fontSize+'px';
   volumeText.style.position = 'absolute';
   volumeText.style['z-index'] = 2147483647; // Always on TOP
@@ -233,7 +233,7 @@ function msgbox(title, message, time, width, height) {
   d.style.height = 'auto';
   d.style.minHeight = h + 'px';
   d.style.backgroundColor = 'white';
-  d.style.color = 'black';
+  d.style.color = 'black !important';
   d.style.border = '2px solid black';
   d.style.zIndex = 2147483647;
   document.body.appendChild(d);
@@ -247,7 +247,7 @@ function msgbox(title, message, time, width, height) {
     titleElement.style.margin = 0;
     titleElement.style.padding = (padding/2) + 'px';
     titleElement.style.background = '#4CAF50';
-    titleElement.style.color = 'white';
+    titleElement.style.color = 'white !important';
     titleElement.innerText = title;
     d.appendChild(titleElement);
   }
