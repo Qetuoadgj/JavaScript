2c9403e740454b3ca99a528278a0ae4286c4dc43
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-09-20 03:58:32 +0300

    Update JS.Functions.Lib.user.js

--- Libs/JS.Functions.Lib.user.js
+++ Libs/JS.Functions.Lib.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         JS.Functions.Lib
-// @version      1.0.0
+// @version      1.0.00
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @match        http://tampermonkey.net/*
