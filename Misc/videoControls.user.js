d5eeee78aa6c5f196f3a88a72bcdf792726d4be7
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-10-25 10:15:48 +0300

    Add files via upload

--- Misc/videoControls.user.js
+++ Misc/videoControls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         videoControls
-/// @icon         https://www.google.com/s2/favicons?domain=youtube.com
-// @version      1.0.1
+// @icon         http://www.worldleathercongress2015.com/images/media_video.png
+// @version      1.0.0
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @grant        none
@@ -10,24 +10,27 @@
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Misc/videoControls.user.js
 // @homepageURL  https://github.com/Qetuoadgj/JavaScript/tree/master/Misc
 // @match        https://*.googlevideo.com/videoplayback?id=*
-// @match        http://*.ex.ua/show/*/*/*.mp4
-// @match        http://*.porndoe.com/movie/*/*/*/*/*/*.mp4?*
-// @match        http://*.eporner.com/*/*/*-*p.mp4
-// @match        http://mcloud.hdporncollections.com/*.mp4
-// @match        http://redirector.rhcdn.net/media/videos/hd/*.mp4
-// @match        http://*.rhcdn.net/media/videos/hd/*.mp4?*
-// @match        https://*.pornhub.com/videos/*/*/*/*.mp4?*
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  function LaunchVideo() {
-    var videoFrame = document.querySelector('body > video');
-    videoFrame.play();
-    videoFrame.volume = 0.5;
-    addMouseWheelAudioControl(videoFrame, 5);
+  // THIS FILE GLOBAL VARIABLES
+  // ====================================================================================================================
+  var pageHost = location.hostname, pageURL = location.href, pageTitle = document.title;
+  var delay = 1000, tries = 15;
+  var videoSourceSelector = 'video > source[type="video/mp4"], video';
+  var waitGroup = []; // waitForElement() timers group.
+  // ====================================================================================================================
+  function applyVideoSettings() {
+    var videoSource =  document.querySelectorAttribute(videoSourceSelector, 'src');
+    var videoPoster = null; //video.poster
+    var videoCleaned = getCleanVideo(videoSource, videoPoster);
+    videoCleaned.play();
+    videoCleaned.volume = 0.5;
+    addMouseWheelAudioControl(videoCleaned, 5);
     useVolumeCookie('body > video', null);
+    waitGroup = null;
   }
-  waitForElement('body > video', false, LaunchVideo, 250, 30);
+  waitForElement(videoSourceSelector, 'src', applyVideoSettings, delay, tries, null, waitGroup);
 })();
