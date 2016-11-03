5eb393b90dd32d209436ba392245dfa8c2fd9509
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-11-03 11:39:42 +0200

    Add files via upload

--- Services/GoogleFavicons.user.js
+++ Services/GoogleFavicons.user.js
@@ -6,7 +6,7 @@
 // @author       Ægir
 // @grant        none
 // @run-at       document-end
-// @require      https://github.com/Qetuoadgj/JavaScript/raw/master/Libs/JS.Functions.Lib.user.js
+/// @require      https://github.com/Qetuoadgj/JavaScript/raw/master/Libs/JS.Functions.Lib.user.js
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/GoogleFavicons.user.js
 // @homepageURL  https://github.com/Qetuoadgj/JavaScript/tree/master/Services
 // @match        https://encrypted.google.com/*
@@ -21,26 +21,28 @@
     for (var count = 0; count < resultsArray.length; ++count) {
       var a = resultsArray[count];
       var r = a.parentNode;
-      var rc = r.parentNode;
-      var host = a.href.replace(/.*https?:\/\//, '').replace(/\/.*$/,'');
+      var favicon = r.querySelector('.favicon');
+      if (!favicon) {
+        // var rc = r.parentNode;
+        var host = a.href.replace(/.*https?:\/\//, '').replace(/\/.*$/,'');
 
-      var i = document.createElement('img');
-      i.style.position = 'relative';
-      i.style.top = '1px';
-      i.style.bottom = '0';
-      i.style.left = '0';
-      i.style.right = '0';
-      // i.style.width = '50%';
-      i.style.height = '16px';
-      i.style.margin = '0px 3px 0px 0px';
-      i.src = 'https://www.google.com/s2/favicons?domain=' + host;
-      i.className = 'favicon';
+        var i = document.createElement('img');
+        i.style.position = 'relative';
+        i.style.top = '1px';
+        i.style.bottom = '0';
+        i.style.left = '0';
+        i.style.right = '0';
+        // i.style.width = '50%';
+        i.style.height = '16px';
+        i.style.margin = '0px 3px 0px 0px';
+        i.src = 'https://www.google.com/s2/favicons?domain=' + host;
+        i.className = 'favicon';
 
-      r.insertBefore(i, a);
-      // r.appendChild(i);
+        r.insertBefore(i, a);
+        // r.appendChild(i);
+      }
     }
-    console.log('resultsArray.length = '+resultsArray.length);
   };
-  waitForElement('div.rc > .r > a', 'href', initFunction, 1000, 15, false);
-
+  // waitForElement('div.rc > .r > a', 'href', initFunction, 1000, 15, false);
+  document.addEventListener('DOMNodeInserted', function handleNewFavicons(event){if (event.target.className != 'favicon') {initFunction();}} , false);
 })();
