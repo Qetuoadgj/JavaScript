eaab0b0b99ac6d397ce3e41bcba48d88ccf90ee2
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-11-03 11:29:02 +0200

    Add files via upload

---
+++ Services/GoogleFavicons.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         GoogleFavicons
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @version      1.0.0
+// @description  Pure JavaScript version.
+// @author       Ægir
+// @grant        none
+// @run-at       document-end
+// @require      https://github.com/Qetuoadgj/JavaScript/raw/master/Libs/JS.Functions.Lib.user.js
+// @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/GoogleFavicons.user.js
+// @homepageURL  https://github.com/Qetuoadgj/JavaScript/tree/master/Services
+// @match        https://encrypted.google.com/*
+// @match        https://google.com/*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  var initFunction = function() {
+    var resultsArray = document.querySelectorAll('div.rc > .r > a');
+    for (var count = 0; count < resultsArray.length; ++count) {
+      var a = resultsArray[count];
+      var r = a.parentNode;
+      var rc = r.parentNode;
+      var host = a.href.replace(/.*https?:\/\//, '').replace(/\/.*$/,'');
+
+      var i = document.createElement('img');
+      // i.style.position = 'absolute';
+      i.style.top = '0';
+      i.style.bottom = '0';
+      i.style.left = '0';
+      i.style.right = '0';
+      // i.style.width = '50%';
+      i.style.height = '16px';
+      i.style.margin = '0px 3px 0px 0px';
+      i.src = 'https://www.google.com/s2/favicons?domain=' + host;
+      i.className = 'favicon';
+
+      r.insertBefore(i, a);
+      // r.appendChild(i);
+    }
+    console.log('resultsArray.length = '+resultsArray.length);
+  };
+  waitForElement('div.rc > .r > a', 'href', initFunction, 1000, 15, false);
+
+})();
