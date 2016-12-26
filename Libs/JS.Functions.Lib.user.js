b3aa9c06808cd46087107c9e9e651267bf6657a7
Qetuoadgj <a.sukhanovskiy@gmail.com>
2016-12-26 23:18:51 +0200

    Add files via upload

--- Libs/JS.Functions.Lib.user.js
+++ Libs/JS.Functions.Lib.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         JS.Functions.Lib
-// @version      1.0.4
+// @version      1.0.5
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @match        http://tampermonkey.net/*
@@ -440,3 +440,26 @@ function inIframe() {
   console.log('inIframe: '+inIframe);
   return inIframe;
 }
+
+/* To Title Case 2.1 – http://individed.com/code/to-title-case/
+* Copyright © 2008–2013 David Gouch. Licensed under the MIT License. */
+
+String.prototype.toTitleCase = function(lower) {
+  var string = lower ? this.toLowerCase() : this;
+  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+
+  return string.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title) {
+    if (index > 0 && index + match.length !== title.length &&
+        match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+        (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+        title.charAt(index - 1).search(/[^\s-]/) < 0) {
+      return match.toLowerCase();
+    }
+
+    if (match.substr(1).search(/[A-Z]|\../) > -1) {
+      return match;
+    }
+
+    return match.charAt(0).toUpperCase() + match.substr(1);
+  });
+};
