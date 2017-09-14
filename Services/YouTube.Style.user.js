40fae5c89c2b42800657a1fcee0bcee453a7f0aa
Qetuoadgj <a.sukhanovskiy@gmail.com>
2017-09-14 09:01:29 +0300

    Add files via upload

--- Services/YouTube.Style.user.js
+++ Services/YouTube.Style.user.js
@@ -7,10 +7,9 @@
 // @grant        none
 // @run-at       document-end
 // @noframes
-// @require      https://github.com/Qetuoadgj/JavaScript/raw/master/Libs/JS.Functions.Lib.user.js
-// @require      https://github.com/Qetuoadgj/JavaScript/raw/master/Libs/JS.AddEmbedCodeFrame.Lib.user.js
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/YouTube.Style.user.js
 // @homepageURL  https://github.com/Qetuoadgj/JavaScript/tree/master/Services
+// @require      https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js
 // @match        https://www.youtube.com/*
 // ==/UserScript==
 
@@ -18,32 +17,27 @@
 	'use strict';
 
 	// Your code here...
-	var G_styleName = 'YouTube_Style';
-	var Fix_Guide_Style = function() {
-		var width = 194 + 20;                                                   // 194
-		addGlobalStyle(
-			'.guide-channels-list a.guide-item {width: '+(width-10)+'px;}'+     // 184
-			'#guide-container > div {width: '+(width)+'px;}'+                   // 194
-			'#appbar-guide-menu {width: '+(230+width-194)+'px;}'+               // 230
-			'#guide .display-name.no-count {width: '+(140+width-194-30)+'px;}', // 140 (-30)
-			G_styleName
-		);
-		addGlobalStyle('.guide-count-value {color: white;}', G_styleName);
-	}();
+	var G_ScriptName = 'YouTube.Style';
+
+	function CSS(name, rule, replace) {
+		var css = name ? $('style[name="' + name + '"]')[0] : null, exists = css ? true : false;
+		if (!exists || (exists && replace)) css = $('<style></style>')[0];
+		if (!exists) $('html > head').append(css);
+		if (name) $(css).attr('name', name);
+		$(css).html($(css).html()+rule);
+		console.log('CSS: ', css);
+		return css;
+	}
+
 	var Fix_ClipConverter_Style = function() {
-		var buttons = document.querySelectorAll('#clipconverter button');
-		for (var index = 0; index < buttons.length; index++) {
-			var button = buttons[index];
-			var span = button.querySelector('span');
-			if (!span) {
-				var text = button.innerHTML;
-				button.innerHTML = '<span class="yt-uix-button-content"><strong>' + text + '</strong></span>';
-			}
-			button.setAttribute('class', 'yt-uix-button yt-uix-button-opacity yt-uix-tooltip');
-		}
+		$('#clipconverter button').each(function( index ) {
+			if (!$(this).find('span')[0]) $(this).html('<span class="yt-uix-button-content"><strong>'+$(this).html()+'</strong></span>');
+			$(this).attr('class', 'yt-uix-button yt-uix-button-opacity yt-uix-tooltip');
+		});
 	}();
+
 	var Fix_MagicOptions_Style = function() {
-		addGlobalStyle('#watch7-content div {position: inherit;}',G_styleName); // Bottom Bar
-		addGlobalStyle('div#ma-cin-panel:hover {opacity: 1;} div#ma-cin-panel {opacity: 0.1;}',G_styleName); // Magic Options Screen Button
+		var style_name = G_ScriptName+'.Fix_MagicOptions_Style';
+		CSS(style_name, ('\n\t'+'#watch7-content > div {position: relative; z-index: 0;}'+'\n'), true);
 	}();
 })();
