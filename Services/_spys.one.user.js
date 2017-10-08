8f0ee27eb7a018d2c97a676708f2a160505765a4
Qetuoadgj <a.sukhanovskiy@gmail.com>
2017-10-09 00:17:06 +0300

    Add files via upload

--- Services/_spys.one.user.js
+++ Services/_spys.one.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         spys.one
 // @icon         https://www.google.com/s2/favicons?domain=spys.one
-// @version      1.0.2
+// @version      1.0.1
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @grant        GM_registerMenuCommand
@@ -9,7 +9,7 @@
 // @grant        GM_getValue
 // @grant        GM_deleteValue
 /// @grant        none
-// @run-at       document-start
+// @run-at       document-end
 /// @noframes
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/spys.one.user.js
 // @homepageURL  https://github.com/Qetuoadgj/JavaScript/tree/master/Services
@@ -40,8 +40,6 @@
 	};
 
 	function pageIsInIframe() {return window.location !== window.parent.location;}
-
-	if (shortURL !== location.protocol + "//" + location.host + "/") window.location = location.protocol + "//" + location.host;
 
 	function check_proxies() {
 		if ( pageURL.match("https://hidemy.name/") ) {
