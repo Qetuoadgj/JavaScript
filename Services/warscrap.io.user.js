ce7b912ecd2f34a7eb448ce05ddd09d016ea1e4c
Qetuoadgj <a.sukhanovskiy@gmail.com>
2018-09-11 04:49:07 +0300

    Add files via upload

--- Services/warscrap.io.user.js
+++ Services/warscrap.io.user.js
@@ -54,20 +54,6 @@
     canvas.style.pointerEvents = 'none'; // click through
 
     var context = canvas.getContext('2d');
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var radius = (canvas.height /2) - lineWidth/2;
-
-    context.globalAlpha = dotOpacity;
-    context.beginPath();
-    radius = dotRadiusPX - lineWidth/2;
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = dotColor;
-    context.fill();
-    context.lineWidth = lineWidth;
-    context.strokeStyle = dotColor;
-    context.stroke();
-    context.globalAlpha = 1.0;
 
     // center
     var x = canvas.width / 2;
@@ -94,29 +80,184 @@
         context.lineTo(x + (distFromCenter + barLen), y);
     }
 
-    var horBarLen = 0;
-    /*
-    for (var i = 0; i < 5; i++) {
-        addVerticalBars(scaleValue*(i+1), smallLineSize);
-        horBarLen += scaleValue;
+    function drawCanvas(canvas) {
+        var horBarLen = 0;
+        /*
+        for (var i = 0; i < 5; i++) {
+            addVerticalBars(scaleValue*(i+1), smallLineSize);
+            horBarLen += scaleValue;
+        }
+        addHorizontalBars(scaleValue/2, horBarLen);
+        */
+
+        context.globalAlpha = lineOpacity;
+        context.beginPath();
+        context.strokeStyle = lineColor1 || 'white';
+        context.lineWidth = lineWidth;
+        addVerticalBars(scaleValue*1, smallLineSize);
+        context.stroke();
+        context.globalAlpha = 1.0;
+
+        context.globalAlpha = lineOpacity;
+        context.beginPath();
+        context.strokeStyle = lineColor2 || 'white';
+        context.lineWidth = lineWidth;
+        addVerticalBars(scaleValue*0.5, smallLineSize*0.25);
+        context.stroke();
+        context.globalAlpha = 1.0;
+
+        /*
+        var centerX = canvas.width / 2;
+        var centerY = canvas.height / 2;
+        var radius = (canvas.height /2) - lineWidth/2;
+        */
+
+        var radius = dotRadiusPX - lineWidth/2;
+        context.globalAlpha = dotOpacity;
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = dotColor;
+        context.fill();
+        context.lineWidth = lineWidth;
+        context.strokeStyle = dotColor;
+        context.stroke();
+        context.globalAlpha = 1.0;
     }
-    addHorizontalBars(scaleValue/2, horBarLen);
-    */
+    drawCanvas(canvas);
 
-    context.globalAlpha = lineOpacity;
-    context.beginPath();
-    context.strokeStyle = lineColor1 || 'white';
-    context.lineWidth = lineWidth;
-    addVerticalBars(scaleValue*1, smallLineSize);
-    context.stroke();
-    context.globalAlpha = 1.0;
+    var KEY_BACKSPACE = 8,
+        KEY_TAB = 9,
+        KEY_ENTER = 13,
+        KEY_SHIFT = 16,
+        KEY_CTRL = 17,
+        KEY_ALT = 18,
+        KEY_PAUSE_BREAK = 19,
+        KEY_CAPS_LOCK = 20,
+        KEY_ESCAPE = 27,
+        KEY_PAGE_UP = 33,
+        KEY_PAGE_DOWN = 34,
+        KEY_END = 35,
+        KEY_HOME = 36,
+        KEY_LEFT_ARROW = 37,
+        KEY_UP_ARROW = 38,
+        KEY_RIGHT_ARROW = 39,
+        KEY_DOWN_ARROW = 40,
+        KEY_INSERT = 45,
+        KEY_DELETE = 46,
+        KEY_0 = 48,
+        KEY_1 = 49,
+        KEY_2 = 50,
+        KEY_3 = 51,
+        KEY_4 = 52,
+        KEY_5 = 53,
+        KEY_6 = 54,
+        KEY_7 = 55,
+        KEY_8 = 56,
+        KEY_9 = 57,
+        KEY_A = 65,
+        KEY_B = 66,
+        KEY_C = 67,
+        KEY_D = 68,
+        KEY_E = 69,
+        KEY_F = 70,
+        KEY_G = 71,
+        KEY_H = 72,
+        KEY_I = 73,
+        KEY_J = 74,
+        KEY_K = 75,
+        KEY_L = 76,
+        KEY_M = 77,
+        KEY_N = 78,
+        KEY_O = 79,
+        KEY_P = 80,
+        KEY_Q = 81,
+        KEY_R = 82,
+        KEY_S = 83,
+        KEY_T = 84,
+        KEY_U = 85,
+        KEY_V = 86,
+        KEY_W = 87,
+        KEY_X = 88,
+        KEY_Y = 89,
+        KEY_Z = 90,
+        KEY_LEFT_WINDOW_KEY = 91,
+        KEY_RIGHT_WINDOW_KEY = 92,
+        KEY_SELECT_KEY = 93,
+        KEY_NUMPAD_0 = 96,
+        KEY_NUMPAD_1 = 97,
+        KEY_NUMPAD_2 = 98,
+        KEY_NUMPAD_3 = 99,
+        KEY_NUMPAD_4 = 100,
+        KEY_NUMPAD_5 = 101,
+        KEY_NUMPAD_6 = 102,
+        KEY_NUMPAD_7 = 103,
+        KEY_NUMPAD_8 = 104,
+        KEY_NUMPAD_9 = 105,
+        KEY_MULTIPLY = 106,
+        KEY_ADD = 107,
+        KEY_SUBTRACT = 109,
+        KEY_DECIMAL_POINT = 110,
+        KEY_DIVIDE = 111,
+        KEY_F1 = 112,
+        KEY_F2 = 113,
+        KEY_F3 = 114,
+        KEY_F4 = 115,
+        KEY_F5 = 116,
+        KEY_F6 = 117,
+        KEY_F7 = 118,
+        KEY_F8 = 119,
+        KEY_F9 = 120,
+        KEY_F10 = 121,
+        KEY_F11 = 122,
+        KEY_F12 = 123,
+        KEY_NUM_LOCK = 144,
+        KEY_SCROLL_LOCK = 145,
+        KEY_SEMI_COLON = 186,
+        KEY_EQUAL_SIGN = 187,
+        KEY_COMMA = 188,
+        KEY_DASH = 189,
+        KEY_PERIOD = 190,
+        KEY_FORWARD_SLASH = 191,
+        KEY_GRAVE_ACCENT = 192,
+        KEY_OPEN_BRACKET = 219,
+        KEY_BACK_SLASH = 220,
+        KEY_CLOSE_BRACKET = 221,
+        KEY_SINGLE_QUOTE = 222
+    ;
 
-    context.globalAlpha = lineOpacity;
-    context.beginPath();
-    context.strokeStyle = lineColor2 || 'white';
-    context.lineWidth = lineWidth;
-    addVerticalBars(scaleValue*0.5, smallLineSize*0.25);
-    context.stroke();
-    context.globalAlpha = 1.0;
+    var toggleSwitch = false;
+    function onKeyDown(e, code) {
+        e = e || window.event;
+        var ctrlDown = e.ctrlKey || e.metaKey; // Mac support
+        var targetType = e.target.tagName.toLowerCase();
+        if (code) e.keyCode = code;
+        if (!(targetType == 'input' || targetType == 'textarea')) {
+            if (e.keyCode == KEY_X) {
+                if (toggleSwitch == false) {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                }
+                else {
+                    drawCanvas(canvas);
+                }
+                toggleSwitch = !toggleSwitch;
+                // e.preventDefault();
+            }
+            else if (e.keyCode == KEY_OPEN_BRACKET) {
+                scaleValue /= 1.1;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                drawCanvas(canvas);
+            }
+            else if (e.keyCode == KEY_CLOSE_BRACKET) {
+                scaleValue *= 1.1;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                drawCanvas(canvas);
+            }
+
+            // e.preventDefault();
+        }
+    }
+
+    // window.onkeydown =  function(e){onKeyDown(e);};
+    window.addEventListener('keydown', function(e){onKeyDown(e);}, false);
 
 })();
