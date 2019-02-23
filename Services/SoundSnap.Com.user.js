e46b4ff54f6e8322ca9dd8325d737937c8088ab3
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-02-23 15:14:33 +0200

    Add files via upload

--- Services/SoundSnap.Com.user.js
+++ Services/SoundSnap.Com.user.js
@@ -17,33 +17,44 @@
     'use strict';
 
     // Your code here...
-    function clearAllEventListeners(element) {
-        var new_element = element.cloneNode(true);
-        element.parentNode.replaceChild(new_element, element);
-    }
-    var keepClassNames = ['si_buttons','si_download']
-    var audio_previews = document.querySelectorAll('.wave > audio');
-    for (var audio of audio_previews) {
-        var link = audio.getAttribute('src');
-        var parent = audio.parentNode;
-        var i = 0; while (i < 10) {
-            if (parent.classList.contains('audio-row')) break;
-            parent = parent.parentNode;
-            i++;
+    function clearAllEventListeners(element) {element.parentNode.replaceChild(element.cloneNode(true), element);}
+    var keepClassNames = ['si_buttons', 'si_download'];
+    var mainTimerCounter = 0, mainTimer = setInterval(function(tries) {
+        mainTimerCounter++;
+        var testCodition = document.querySelector('a.mp3-download');
+        var stopUpdate = (mainTimerCounter >= tries);
+        if (stopUpdate) {
+            clearInterval(mainTimer);
+            alert('mainTimer: fail');
         }
-        var download_buttons = parent.querySelectorAll('a.si_download');
-        for (var button of download_buttons) {
-            button.setAttribute('href', link);
-            button.removeAttribute('onclick');
-            button.setAttribute('target', '_blank');
-            button.setAttribute('rel', 'noopener noreferrer');
-            for (var className of button.classList) {
-                if (keepClassNames.contains(className)) continue;
-                button.classList.remove(className);
+        else if (testCodition) {
+            var audio_previews = document.querySelectorAll('.wave > audio');
+            for (var audio of audio_previews) {
+                var link = audio.getAttribute('src');
+                var parent = audio.parentNode;
+                var i = 0; while (i < 10) {
+                    if (parent.classList.contains('audio-row')) break;
+                    parent = parent.parentNode;
+                    i++;
+                }
+                var download_buttons = parent.querySelectorAll('a.si_download');
+                for (var button of download_buttons) {
+                    button.setAttribute('href', link);
+                    button.removeAttribute('onclick');
+                    button.setAttribute('target', '_blank');
+                    button.setAttribute('rel', 'noopener noreferrer');
+                    for (var className of button.classList) {
+                        if (keepClassNames.includes(className)) continue;
+                        button.classList.remove(className);
+                    }
+                    clearAllEventListeners(button);
+                }
+                // console.log(parent);
+                // console.log(audio);
             }
-            clearAllEventListeners(button);
+            clearInterval(mainTimer);
+            // alert('mainTimer: success');
         }
-        // console.log(parent);
-        // console.log(audio);
-    }
-})();
+        console.log('mainTimerCounter:', mainTimerCounter);
+    }, 250, 50)
+    })();
