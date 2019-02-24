67016e2b1b6066b8b4877919fe5bf717256c6339
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-02-24 18:32:53 +0200

    Add files via upload

--- Services/hdrezka.ag.user.js
+++ Services/hdrezka.ag.user.js
@@ -12,7 +12,11 @@
 // @match        *://hdrezka.ag/series/*.html
 // @match        *://hdrezka.me/films/*.html
 // @match        *://hdrezka.me/series/*.html
-// @grant        none
+//// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 /* uBlock Filter:
@@ -32,6 +36,9 @@ hdrezka.*##.b-post__mixedtext
     'use strict';
 
     // Your code here...
+    var nightModeEnable = GM_getValue('nightModeEnable', 1);
+    var autoScaleEnable = GM_getValue('autoScaleEnable', 1);
+
     function addGlobalStyle(css, cssClass) {
         var head = document.getElementsByTagName('head')[0]; if (!head) { return; }
         var style = document.createElement('style'); style.type = 'text/css'; style.innerHTML = css;
@@ -50,31 +57,56 @@ hdrezka.*##.b-post__mixedtext
         var style = document.querySelector('head > style.zoomMode'); if (style) style.remove();
         style = addGlobalStyle("#player {zoom: "+(scale)+"; z-index: 10; padding: 0;}\nbody.active-brand #wrapper, .b-wrapper {width: "+Math.max(640*scale, 1000)+"px; padding: 10px 20px;}", "zoomMode");
     }
-    scalePlayer(scale);
+    // scalePlayer(scale);
+    function toggleAutoScale() {
+        autoScaleEnable = !autoScaleEnable;
+        if (autoScaleEnable) {
+            playerElement = document.querySelector('#videoplayer');
+            playerWidth = playerElement ? playerElement.clientWidth : 640;
+            playerHeight = playerElement ? playerElement.clientHeight : 360;
+            initScale = Math.min((window.innerWidth - 20*2 - 16) / playerWidth, (window.innerHeight - 10*2 - 8) / playerHeight);
+            minScale = 1;
+            maxScale = initScale; // 2.5;
+            scale = initScale; scale = Math.min(Math.max(minScale, scale), maxScale);
+        }
+        else {
+            scale = 1;
+        }
+        scalePlayer(scale);
+        GM_setValue('autoScaleEnable', autoScaleEnable);
+    }
     //
-    function toggleNightMode() {
+    function toggleNightMode(force) {
+        var css = "div, body, .night_mode, body.active-brand #wrapper {background: black !important; background-color: black !important}" +
+            "\nh2, .b-post__description_text, td, .misc, div#hd-comments-list * {color: wheat;}"+
+            "\n/*.b-post__social_holder_wrapper, table.b-post__rating_table, table.b-post__actions {display: none;}*/" +
+            "\n::-webkit-scrollbar {width: 18px; height: 18px;}" +
+            "\n::-webkit-scrollbar-button {background: no-repeat #222; background-size: 18px; background-position: center bottom;}" +
+            "\n::-webkit-scrollbar-button:vertical:decrement {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='25,66 50,33 75,66'/></svg>" +'");}' +
+            "\n::-webkit-scrollbar-button:vertical:increment {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='25,33 50,66 75,33'/></svg>" +'");}' +
+            "\n::-webkit-scrollbar-button:horizontal:decrement {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='33,50 66,75 66,25'/></svg>" +'");}' +
+            "\n::-webkit-scrollbar-button:horizontal:increment {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='33,75 66,50 33,25'/></svg>" +'");}' +
+            "\n::-webkit-scrollbar-track-piece {background: #111;}" +
+            "\n::-webkit-scrollbar-thumb {background: #222; border-radius: 3px;}" +
+            "\n::-webkit-scrollbar-thumb:hover {background: #444;}" +
+            "\n::-webkit-scrollbar-corner {background-color: #111;}" +
+            //
+            "\niframe#cdn-player {border: 1px #111 solid;}"
+        ;
         document.body.classList.add('night_mode');
         var style = document.querySelector('head > style.nightMode');
-        if (style) style.remove();
-        else {
-            var css = "div, body, .night_mode, body.active-brand #wrapper {background: black !important; background-color: black !important}" +
-                "\nh2, .b-post__description_text, td, .misc, div#hd-comments-list * {color: wheat;}"+
-                "\n/*.b-post__social_holder_wrapper, table.b-post__rating_table, table.b-post__actions {display: none;}*/" +
-                "\n::-webkit-scrollbar {width: 18px; height: 18px;}" +
-                "\n::-webkit-scrollbar-button {background: no-repeat #222; background-size: 18px; background-position: center bottom;}" +
-                "\n::-webkit-scrollbar-button:vertical:decrement {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='25,66 50,33 75,66'/></svg>" +'");}' +
-                "\n::-webkit-scrollbar-button:vertical:increment {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='25,33 50,66 75,33'/></svg>" +'");}' +
-                "\n::-webkit-scrollbar-button:horizontal:decrement {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='33,50 66,75 66,25'/></svg>" +'");}' +
-                "\n::-webkit-scrollbar-button:horizontal:increment {background-image: url(" + '"' + "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%235a6268'><polygon points='33,75 66,50 33,25'/></svg>" +'");}' +
-                "\n::-webkit-scrollbar-track-piece {background: #111;}" +
-                "\n::-webkit-scrollbar-thumb {background: #222; border-radius: 3px;}" +
-                "\n::-webkit-scrollbar-thumb:hover {background: #444;}" +
-                "\n::-webkit-scrollbar-corner {background-color: #111;}"
-            ;
-            addGlobalStyle(css, "nightMode");
+        if (force && style) style.remove();
+        if (style) {
+            style.remove();
+            nightModeEnable = 0;
         }
+        else {
+            addGlobalStyle(css, "nightMode");
+            nightModeEnable = 1;
+        }
+        GM_setValue('nightModeEnable', nightModeEnable);
     }
-    toggleNightMode();
+    if (nightModeEnable) toggleNightMode(1);
     //
     var KEY_BACKSPACE = 8, KEY_TAB = 9, KEY_ENTER = 13, KEY_SHIFT = 16, KEY_CTRL = 17, KEY_ALT = 18, KEY_PAUSE_BREAK = 19, KEY_CAPS_LOCK = 20, KEY_ESCAPE = 27, KEY_PAGE_UP = 33, KEY_PAGE_DOWN = 34, KEY_END = 35, KEY_HOME = 36, KEY_LEFT_ARROW = 37, KEY_UP_ARROW = 38, KEY_RIGHT_ARROW = 39, KEY_DOWN_ARROW = 40, KEY_INSERT = 45, KEY_DELETE = 46, KEY_0 = 48, KEY_1 = 49, KEY_2 = 50, KEY_3 = 51, KEY_4 = 52, KEY_5 = 53, KEY_6 = 54, KEY_7 = 55, KEY_8 = 56, KEY_9 = 57, KEY_A = 65, KEY_B = 66, KEY_C = 67, KEY_D = 68, KEY_E = 69, KEY_F = 70, KEY_G = 71, KEY_H = 72, KEY_I = 73, KEY_J = 74, KEY_K = 75, KEY_L = 76, KEY_M = 77, KEY_N = 78, KEY_O = 79, KEY_P = 80, KEY_Q = 81, KEY_R = 82, KEY_S = 83, KEY_T = 84, KEY_U = 85, KEY_V = 86, KEY_W = 87, KEY_X = 88, KEY_Y = 89, KEY_Z = 90, KEY_LEFT_WINDOW_KEY = 91, KEY_RIGHT_WINDOW_KEY = 92, KEY_SELECT_KEY = 93, KEY_NUMPAD_0 = 96, KEY_NUMPAD_1 = 97, KEY_NUMPAD_2 = 98, KEY_NUMPAD_3 = 99, KEY_NUMPAD_4 = 100, KEY_NUMPAD_5 = 101, KEY_NUMPAD_6 = 102, KEY_NUMPAD_7 = 103, KEY_NUMPAD_8 = 104, KEY_NUMPAD_9 = 105, KEY_MULTIPLY = 106, KEY_ADD = 107, KEY_SUBTRACT = 109, KEY_DECIMAL_POINT = 110, KEY_DIVIDE = 111, KEY_F1 = 112, KEY_F2 = 113, KEY_F3 = 114, KEY_F4 = 115, KEY_F5 = 116, KEY_F6 = 117, KEY_F7 = 118, KEY_F8 = 119, KEY_F9 = 120, KEY_F10 = 121, KEY_F11 = 122, KEY_F12 = 123, KEY_NUM_LOCK = 144, KEY_SCROLL_LOCK = 145, KEY_SEMI_COLON = 186, KEY_EQUAL_SIGN = 187, KEY_COMMA = 188, KEY_DASH = 189, KEY_PERIOD = 190, KEY_FORWARD_SLASH = 191, KEY_GRAVE_ACCENT = 192, KEY_OPEN_BRACKET = 219, KEY_BACK_SLASH = 220, KEY_CLOSE_BRACKET = 221, KEY_SINGLE_QUOTE = 222;
     var showCanvas = true;
@@ -102,6 +134,7 @@ hdrezka.*##.b-post__mixedtext
                 e.preventDefault();
             }
             else if (shiftDown && e.keyCode == KEY_LEFT_ARROW) {
+                /*
                 playerElement = document.querySelector('#videoplayer');
                 playerWidth = playerElement ? playerElement.clientWidth : 640;
                 // initScale = 2.0 * window.innerWidth/(1200 + 50);
@@ -110,10 +143,15 @@ hdrezka.*##.b-post__mixedtext
                 scale = initScale;
                 scale = Math.min(Math.max(minScale, scale), maxScale);
                 scalePlayer(scale);
+                */
+                toggleAutoScale();
                 e.preventDefault();
             }
         }
     }
 
     window.addEventListener('keydown', function(e){onKeyDown(e);}, false);
+
+    GM_registerMenuCommand('Night Mode', toggleNightMode, "");
+    GM_registerMenuCommand('Auto Scale', toggleAutoScale, "");
 })();
