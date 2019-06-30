28a72aa8a784ae976ee38169e2ede97a45affff2
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-06-30 17:59:53 +0300

    Add files via upload

--- Services/krunker.io.user.js
+++ Services/krunker.io.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         krunker.io
 // @icon         https://www.google.com/s2/favicons?domain=krunker.io
-// @version      1.0.6
+// @version      1.0.7
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/krunker.io.user.js
@@ -39,91 +39,14 @@
         localStorage.color = localStorage.color || localStorage.kro_setngss_crosshairShadow || "#ff0000";
     };
     initSettings();
-    // document.addEventListener('DOMContentLoaded', function(){
-    var parentElement = 0; parentElement = document.querySelector('.icon[role=presentation]');
-    parentElement = document.querySelector('#gameContainer > canvas');
-    var scale_radius = 1.0 * 0.5; //0.6;
-    var scale_thickness = 1.1;
     //
-    var showCanvas = true;
-    //
-    var showCanvasBorders = 0;
-    var lineWidth = 1.0;
-    var globalOpacity = 1;
-    var lineColor = "orange";
-    var scaleValue = 24;
-    var showCicleMark = 0; // 1
-    var cicleMarkGap = 0.4;
-    var cicleMark = [0.50 + cicleMarkGap / 2, 2.50 - cicleMarkGap / 2];
-    var dotRadiusPX = 6; //1.25*1.25;
-    var dotColor = lineColor;
-    var dotOpacity = 1;
-    var dotFill = 0; // 1
-    var shade = 1;
-    //
-    showCicleMark = 1;
-    cicleMarkGap = 0.4;
-    scaleValue = 24;
-    lineWidth = 1.2;
-    lineColor = "Orange";
-    dotColor = "Orange";
-    dotRadiusPX = 1.0;
-    dotFill = 1;
-    //
-    showCicleMark = 1;
-    cicleMarkGap = 0.4;
-    scaleValue = 24 * scale_radius;
-    lineWidth = 1.5 * scale_thickness;
-    lineColor = "Orange";
-    dotColor = "Orange";
-    dotRadiusPX = 1.0 + lineWidth / 2 * 1 * 3;
-    dotFill = 0;
-    globalOpacity = 1 ; //* 0.75;
-    shade = 1; // * 0;
-    //
-    var mode = 0;
-    showCicleMark = 1;
-    //
-    lineColor = localStorage.kro_setngss_crosshairColor;
-    dotColor = localStorage.kro_setngss_crosshairColor;
-    //
-    lineColor = "#00ff00";
-    dotColor = "#00ff00";
-    //
-    lineColor = "#FFffaa";
-    dotColor = "#ff2222";
-    //
-    cicleMark = [0.50 + cicleMarkGap / 2, 2.50 - cicleMarkGap / 2];
-    // Your code here...
-    var canvas_id = "canvas_1";
-    var canvas = document.getElementById(canvas_id);
-    if (canvas) { canvas.remove(); canvas = null; }
-    if (!canvas) {
-        canvas = document.createElement("canvas");
-        document.body.appendChild(canvas);
-        canvas.id = canvas_id;
+    function clearCanvas(canvas) // CLEAR CANVAS
+    {
+        let context = canvas.getContext('2d'); // Get context
+        context.clearRect(0, 0, canvas.width, canvas.height);
     };
-    canvas.style.display = 'inline-block';
-    canvas.style.position = 'absolute';
-    canvas.style.zIndex = '2147483648';
-    canvas.style.boxSizing = 'border-box';
-    canvas.style.top = '50%';
-    canvas.style.left = '50%';
-    canvas.style.transform = 'translate(-50%, -50%)';
-    // canvas.style.width = '250px';
-    // canvas.style.height = '250px';
-    canvas.width = Math.min(window.innerWidth, window.innerHeight) - 1; // defaultRadius;
-    canvas.height = canvas.width; //window.innerHeight-1; //defaultRadius;
-    if (showCanvasBorders) {
-        canvas.style.border = '10px';
-        canvas.style.borderColor = 'red';
-        canvas.style.borderStyle = 'solid';
-        canvas.style.borderWidth = 'thin';
-    };
-    canvas.style.opacity = globalOpacity;
-    canvas.style.pointerEvents = 'none'; // click through
-    //
-    function drawArc(context, x, y, radius, angleStart, angleEnd, lineColor, lineWidth, fill, fillColor, opacity) {
+    function drawArc(context, x, y, radius, angleStart, angleEnd, lineColor, lineWidth, fill, fillColor, opacity) // DRAW ARC
+    {
         context.globalAlpha = opacity;
         context.beginPath();
         context.arc(x, y, radius, angleStart, angleEnd, false);
@@ -134,9 +57,9 @@
         context.lineCap = "round";
         context.stroke();
         context.globalAlpha = 1.0;
-    }
-    //
-    function drawChevron(context, x, y, radius, angleStart, angleEnd, lineColor, lineWidth, fill, fillColor, opacity) {
+    };
+    function drawChevron(context, x, y, radius, angleStart, angleEnd, lineColor, lineWidth, fill, fillColor, opacity) // DRAW CHEVRON
+    {
         context.globalAlpha = opacity;
         context.beginPath();
         context.moveTo(x-radius/(2*Math.sqrt(2)), y+radius/2);
@@ -148,53 +71,8 @@
         context.lineCap = "round";
         context.stroke();
         context.globalAlpha = 1.0;
-    }
-    //
-    function drawCanvas(canvas, parentElement) {
-        // Get context
-        var context = canvas.getContext('2d');
-        // center
-        var x = canvas.width / 2;
-        var y = canvas.height / 2;
-        // remove aliasing
-        x = Math.floor(x) + 0.5;
-        y = Math.floor(y) + 0.5;
-        //
-        context.strokeWidth = lineWidth;
-        //
-        var radius = dotRadiusPX/2 ;//lineWidth / 2 ; //dotRadiusPX - lineWidth / 2;
-        // if (shade) drawArc(context, x, y, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, "Red", lineWidth * 1, 0, "Red", dotOpacity * 0.5);
-        // drawArc(context, x, y, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, dotColor, lineWidth, 0, dotColor, dotOpacity);
-        if (shade) drawArc(context, x, y, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, localStorage.color, lineWidth * 2.0, 0, "Red", dotOpacity * 0.5);
-        drawArc(context, x, y, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, /*"White"*/ dotColor, lineWidth, 0, lineColor, dotOpacity);
-        if (mode == 1) {
-            var offsetY = 4;
-            radius = scaleValue * 0.50 * 2; // * 2; // - o.lineWidth/2; // 10 * 1.5; //o.dotRadiusPX - o.lineWidth/2;
-            if (shade) drawChevron(context, x, y+offsetY, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, "Red", lineWidth * 2.0, 0, "Red", dotOpacity * 0.5);
-            drawChevron(context, x, y+offsetY, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, lineColor, lineWidth, 0, lineColor, dotOpacity);
-            showCicleMark = 0;
-        }
-        if (showCicleMark) {
-            radius = scaleValue * 0.50 * 2; // - o.lineWidth/2; // 10 * 1.5; //o.dotRadiusPX - o.lineWidth/2;
-            if (shade) drawArc(context, x, y, radius, cicleMark[0] * Math.PI, cicleMark[1] * Math.PI, localStorage.color, lineWidth * 2.5, 0, "Red", dotOpacity * 0.5);
-            drawArc(context, x, y, radius, cicleMark[0] * Math.PI, cicleMark[1] * Math.PI, lineColor, lineWidth, 0, lineColor, dotOpacity);
-        }
-        if (parentElement) {
-            var rect = parentElement.getBoundingClientRect();
-            var rect_center_x = rect.left + rect.width/2, rect_center_y = rect.top + rect.height/2;
-            canvas.style.left = rect_center_x + "px";
-            canvas.style.top = rect_center_y + "px";
-            console.log(rect_center_x+', '+rect_center_y);
-            // canvas.style.zoom = 1;
-        }
-    }
-    function clearCanvas(canvas) {
-        // Get context
-        var context = canvas.getContext('2d');
-        //
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    //
+    };
+    // Your code here...
     var KEY_BACKSPACE = 8,
         KEY_TAB = 9,
         KEY_ENTER = 13,
@@ -292,52 +170,140 @@
         KEY_OPEN_BRACKET = 219,
         KEY_BACK_SLASH = 220,
         KEY_CLOSE_BRACKET = 221,
-        KEY_SINGLE_QUOTE = 222;
-    //
-    function onKeyDown(e, code) {
-        e = e || window.event;
-        var ctrlDown = e.ctrlKey || e.metaKey; // Mac support
-        var shiftDown = !!window.event.shiftKey;
-        var targetType = e.target.tagName.toLowerCase();
-        if (code) e.keyCode = code;
-        if (!(targetType == 'input' || targetType == 'textarea')) {
-            if (e.keyCode == KEY_ADD) {
-                // console.log("showCanvas: " + showCanvas);
-                if (showCanvas == false) {
-                    clearCanvas(canvas);
-                } else {
-                    clearCanvas(canvas);
-                    drawCanvas(canvas, parentElement);
-                }
-                showCanvas = !showCanvas;
-                e.preventDefault();
-            }
-        }
-    }
-    if (!window.statusReady || typeof window.statusReady == "undefined") {
-        window.addEventListener('keydown', function(e) {
-            onKeyDown(e);
-        }, false);
-        window.statusReady = true;
-    }
-    // LOAD MODS
-    setTimeout(function() {
-        var canLoad = function() {
-            let ui = document.querySelector('#inGameUI');
-            return (ui && ui.style.display !== 'none');
-        };
-        if (canLoad() && localStorage.kro_setngss_canLoadMods == 'true') {
-            initSettings();
-            if (showCanvas) {drawCanvas(canvas, parentElement); showCanvas = !showCanvas;}
-            window.loadUserMod("RUST_SOUNDS", "https://www.dropbox.com/s/ppiqggky4iuvhtf/Rust%20Mod.zip?dl=1", "2624"); // RUST SOUNDS
-        };
-    }, 2500);
-    // });
-    var counter1 = 0, iterator1 = setInterval(function() {
-        if (counter1 > 2) {
-            initSettings();
-            clearInterval(iterator1);
-        };
-        counter1++;
-    }, 2500);
+        KEY_SINGLE_QUOTE = 222
+    ;
+    document.addEventListener(
+        'DOMContentLoaded', function() // ON PAGE LOADED
+        {
+            var canLoad = function() {
+                let ui = document.querySelector('#inGameUI');
+                return (ui && ui.style.display !== 'none');
+            };
+            setTimeout(function() {
+                if (canLoad()) {
+                    /* PREPARE CANVAS PARAMETERS */
+                    var parentElement = null;
+                    parentElement = document.querySelector('.icon[role=presentation]');
+                    parentElement = document.querySelector('#gameContainer > canvas');
+                    var markScaleMult = 0.50;
+                    var markThicknessMult = 1.10;
+                    var showCanvas = true;
+                    var showCanvasBorders = true;
+                    var lineWidth = 1.50 * markThicknessMult;
+                    var globalOpacity = 1.00;
+                    var lineColor = "#FFffaa";
+                    var scaleValue = 24 * markScaleMult;
+                    var showCicleMark = true;
+                    var cicleMarkGap = 0.40;
+                    var dotRadiusPX = 1.00 + lineWidth*1.50;
+                    var dotColor = "#ff2222";
+                    var dotOpacity = 1.00;
+                    var dotFill = false;
+                    var shade = true;
+                    var mode = 0;
+                    /* CREATE PREPARE CANVAS FUNCTION */
+                    function prepareCanvas() {
+                        var canvas_id = "canvas_1";
+                        var canvas = document.getElementById(canvas_id);
+                        if (canvas) {canvas.remove(); canvas = null;}
+                        else {
+                            canvas = document.createElement("canvas");
+                            document.body.appendChild(canvas);
+                            canvas.id = canvas_id;
+                        };
+                        canvas.style.display = 'inline-block';
+                        canvas.style.position = 'absolute';
+                        canvas.style.zIndex = '2147483648';
+                        canvas.style.boxSizing = 'border-box';
+                        canvas.style.top = '50%';
+                        canvas.style.left = '50%';
+                        canvas.style.transform = 'translate(-50%, -50%)';
+                        // canvas.style.width = '250px';
+                        // canvas.style.height = '250px';
+                        canvas.width = Math.min(window.innerWidth, window.innerHeight) - 1; // defaultRadius;
+                        canvas.height = canvas.width; //window.innerHeight-1; //defaultRadius;
+                        if (showCanvasBorders) {
+                            canvas.style.border = '10px';
+                            canvas.style.borderColor = 'red';
+                            canvas.style.borderStyle = 'solid';
+                            canvas.style.borderWidth = 'thin';
+                        };
+                        canvas.style.opacity = globalOpacity;
+                        canvas.style.pointerEvents = 'none'; // click through
+                        return canvas;
+                    };
+                    /* PREPARE CANVAS */
+                    var canvas = prepareCanvas();
+                    /* CREATE DRAW MARK FUNCTION */
+                    function drawCanvas(canvas, parentElement) {
+                        // Get context
+                        var context = canvas.getContext('2d');
+                        // center
+                        var x = canvas.width / 2;
+                        var y = canvas.height / 2;
+                        // remove aliasing
+                        x = Math.floor(x) + 0.5;
+                        y = Math.floor(y) + 0.5;
+                        //
+                        var radius = dotRadiusPX / 2, arc = [0.50 + cicleMarkGap / 2, 2.50 - cicleMarkGap / 2];
+                        context.strokeWidth = lineWidth;
+                        //
+                        if (shade) drawArc(context, x, y, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, localStorage.color, lineWidth * 2.0, 0, "Red", dotOpacity * 0.5);
+                        drawArc(context, x, y, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, /*"White"*/ dotColor, lineWidth, 0, lineColor, dotOpacity);
+                        if (mode == 1) {
+                            var offsetY = 4;
+                            radius = scaleValue;
+                            if (shade) drawChevron(context, x, y+offsetY, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, "Red", lineWidth * 2.0, 0, "Red", dotOpacity * 0.5);
+                            drawChevron(context, x, y+offsetY, radius, (0.50 + 0 / 2) * Math.PI, (2.50 - 0 / 2) * Math.PI, lineColor, lineWidth, 0, lineColor, dotOpacity);
+                            showCicleMark = 0;
+                        };
+                        if (showCicleMark) {
+                            radius = scaleValue;
+                            if (shade) drawArc(context, x, y, radius, arc[0] * Math.PI, arc[1] * Math.PI, localStorage.color, lineWidth * 2.5, 0, "Red", dotOpacity * 0.5);
+                            drawArc(context, x, y, radius, arc[0] * Math.PI, arc[1] * Math.PI, lineColor, lineWidth, 0, lineColor, dotOpacity);
+                        };
+                        if (parentElement) {
+                            var rect = parentElement.getBoundingClientRect();
+                            var rect_center_x = rect.left + rect.width/2, rect_center_y = rect.top + rect.height/2;
+                            canvas.style.left = rect_center_x + "px";
+                            canvas.style.top = rect_center_y + "px";
+                            console.log(rect_center_x+', '+rect_center_y);
+                            // canvas.style.zoom = 1;
+                        };
+                    };
+                    /* INIT CANVAS */
+                    if (showCanvas) {
+                        drawCanvas(canvas, parentElement);
+                        showCanvas = !showCanvas;
+                    };
+                    /* ASSIGN HOTKEYS */
+                    function onKeyDown(e, code) {
+                        e = e || window.event;
+                        var ctrlDown = e.ctrlKey || e.metaKey; // Mac support
+                        var shiftDown = !!window.event.shiftKey;
+                        var targetType = e.target.tagName.toLowerCase();
+                        if (code) e.keyCode = code;
+                        if (!(targetType == 'input' || targetType == 'textarea')) {
+                            if (e.keyCode == KEY_ADD) {
+                                // console.log("showCanvas: " + showCanvas);
+                                if (showCanvas == false) {
+                                    clearCanvas(canvas);
+                                } else {
+                                    clearCanvas(canvas);
+                                    drawCanvas(canvas, parentElement);
+                                }
+                                showCanvas = !showCanvas;
+                                e.preventDefault();
+                            }
+                        }
+                    };
+                    /* INIT HOTKEYS */
+                    window.addEventListener('keydown', function(e) {onKeyDown(e);}, false);
+                    /* LOAD MODS */
+                    if (localStorage.kro_setngss_canLoadMods == 'true') {
+                        window.loadUserMod("RUST_SOUNDS", "https://www.dropbox.com/s/ppiqggky4iuvhtf/Rust%20Mod.zip?dl=1", "2624"); // RUST SOUNDS
+                    };
+                };
+            }, 2500);
+        });
 })();
