9e102a1c57ce15e09122a398773493157525cc75
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-07-22 18:10:33 +0300

    Add files via upload

--- Services/krunker.io.user.js
+++ Services/krunker.io.user.js
@@ -120,6 +120,7 @@
     var png_scope_PU = 'https://github.com/Qetuoadgj/JavaScript/raw/master/Services/Images/PU_sight_Reticle.png'; // 'https://upload.wikimedia.org/wikipedia/commons/2/24/PU_sight_Reticle.png';
     var png_sight_Red_Cross = 'https://raw.githubusercontent.com/Qetuoadgj/JavaScript/master/Services/Images/target-transparent-reticle-3.png'; // 'https://ui-ex.com/images/target-transparent-reticle-3.png';
     var png_sight_Green_Cross = 'https://github.com/Qetuoadgj/JavaScript/raw/master/Services/Images/target-transparent-reticle-3%20%5Bgreen%5D.png';
+    var png_sight_Orange_Cross = 'https://github.com/Qetuoadgj/JavaScript/raw/master/Services/Images/target-transparent-reticle-3%20%5Borange%5D.png';
 
     function initSettings() {
         // Your code here...
@@ -145,7 +146,7 @@
         // localStorage.color = "#000000";
         localStorage.color = localStorage.color || localStorage.kro_setngss_crosshairShadow || "#ff0000";
         localStorage.kro_setngss_customScope = png_scope_PU;
-        localStorage.kro_setngss_customADSDot = png_sight_Red_Cross; // png_sight_Green_Cross; // png_sight_Red_Cross;
+        localStorage.kro_setngss_customADSDot = png_sight_Orange_Cross; // png_sight_Green_Cross; // png_sight_Red_Cross;
         //
         localStorage.kro_setngss_crosshairSho = "3";
         localStorage.kro_setngss_crosshairStyle = "1";
