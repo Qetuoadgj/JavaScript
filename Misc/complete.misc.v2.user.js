ddf9edcd94fee6500ce65886a25cbd69ead2460b
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-08-22 05:03:59 +0300

    Add files via upload

--- Misc/complete.misc.v2.user.js
+++ Misc/complete.misc.v2.user.js
@@ -2,7 +2,7 @@
 // @name         complete.misc.v2
 // @icon         https://www.google.com/s2/favicons?domain=jquery.com
 // @namespace    complete.misc
-// @version      2.0.20
+// @version      2.0.21
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Misc/complete.misc.v2.user.js
