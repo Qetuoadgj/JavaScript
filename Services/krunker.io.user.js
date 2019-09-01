45a30f28ee3652db883ea72c23d6bbc40ba7e704
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-09-01 20:11:30 +0300

    Add files via upload

--- Services/krunker.io.user.js
+++ Services/krunker.io.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         krunker.io
 // @icon         https://www.google.com/s2/favicons?domain=krunker.io
-// @version      1.0.16
+// @version      1.0.18
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/krunker.io.user.js
@@ -129,15 +129,16 @@
     var Reticle_RED = 'https://drive.google.com/open?id=11SoQbfgg-2Exq3Nxn7jQdribTfgxJrjR';
     var Reticle_Scope_PU = 'https://drive.google.com/file/d/19OzzNGLKh7RmlcQqW3cpUPqBkMc_-xSm/view?usp=sharing';
 
+    var png_crosshair = googl('https://drive.google.com/open?id=1kuuMyJiALyQrMTNNUi20CKSDfwMv-yD5');
     var png_sight_dot = googl(Reticle_RED);
     var png_sight_sniper = googl(Reticle_Scope_PU);
 
     function initSettings() {
-		let ON = 'true',
+        let ON = 'true',
             OFF = 'false'
         ;
         // Your code here...
-        localStorage.classindex = localStorage.classindex || 2;
+        localStorage.classindex = localStorage.classindex || 12;
         localStorage.consent = localStorage.consent || 1;
         localStorage.krk_record = localStorage.krk_record || OFF;
         localStorage.kro_setngss_sensitivity = localStorage.kro_setngss_sensitivity || 1.60;
@@ -147,7 +148,7 @@
         localStorage.kro_setngss_crosshairColor = localStorage.kro_setngss_crosshairColor || "#ffffff"; // "#ff8040"; // "#ff8000"; // "#00ff00";
         localStorage.kro_setngss_crosshairShadow = "#000000"; // "#ff0000";
         localStorage.kro_setngss_fov = localStorage.kro_setngss_fov || 85;
-        localStorage.kro_setngss_fpsFOV = localStorage.kro_setngss_fpsFOV || localStorage.kro_setngss_fov + 5; // 120;
+        localStorage.kro_setngss_fpsFOV = /* localStorage.kro_setngss_fpsFOV || */ localStorage.kro_setngss_fov*1 + 5; // 120;
         localStorage.kro_setngss_muzzleFlash = localStorage.kro_setngss_muzzleFlash || OFF;
         localStorage.kro_setngss_particles = localStorage.kro_setngss_particles || OFF;
         localStorage.kro_setngss_resolution = localStorage.kro_setngss_resolution || 1;
@@ -159,19 +160,25 @@
         // localStorage.color = "#000000";
         localStorage.color = localStorage.color || localStorage.kro_setngss_crosshairShadow || "#ff0000";
         localStorage.kro_setngss_customScope = png_sight_sniper;
-        // localStorage.kro_setngss_customADSDot = png_sight_dot;
+        localStorage.kro_setngss_customADSDot = png_sight_dot;
         //
+        localStorage.kro_setngss_crosshairAlways = OFF;
         localStorage.kro_setngss_crosshairSho = 3;
         localStorage.kro_setngss_crosshairStyle = 1;
-        //
+        /*
+        localStorage.kro_setngss_crosshairAlways = ON;
+        localStorage.kro_setngss_crosshairImage = png_crosshair;
+        localStorage.kro_setngss_crosshairSho = 4;
+        localStorage.kro_setngss_crosshairStyle = 1;
+        */
         localStorage.kro_setngss_showDeaths = ON;
         // localStorage.kro_setngss_showTrails = "false";
         // localStorage.kro_setngss_particles = "true";
         //
         // localStorage.kro_setngss_crosshairSho = "3";
-		//
-		localStorage.kro_setngss_dynamicHP = OFF;
-		localStorage.kro_setngss_showUnboxings = OFF;
+        //
+        localStorage.kro_setngss_dynamicHP = OFF;
+        localStorage.kro_setngss_showUnboxings = OFF;
     };
     initSettings();
     //
@@ -396,10 +403,11 @@
                 /* LOAD MODS */
                 if (localStorage.kro_setngss_canLoadMods == 'true') {
                     let modsArray = [
-                        // ["NoSight", "https://www.dropbox.com/s/8kojf1bjy6djzbq/NoSight.zip?dl=0", "2723"],
+                        ["NoSight", "https://www.dropbox.com/s/8kojf1bjy6djzbq/NoSight.zip?dl=0", "2723"],
                         // ["RUST_SOUNDS", "https://www.dropbox.com/s/ppiqggky4iuvhtf/Rust%20Mod.zip?dl=1", "2624"],
                     ];
                     loadMods(modsArray, 1000);
+                    // localStorage.krk_lastMod = 'https://www.dropbox.com/s/8kojf1bjy6djzbq/NoSight.zip?dl=0';
                 };
             };
         }, 2500);
