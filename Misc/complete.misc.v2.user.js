c049f9dfb30868cd743bd31e6b44d35024faa386
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-10-05 18:33:05 +0300

    Add files via upload

--- Misc/complete.misc.v2.user.js
+++ Misc/complete.misc.v2.user.js
@@ -2,7 +2,7 @@
 // @name         complete.misc.v2
 // @icon         https://www.google.com/s2/favicons?domain=jquery.com
 // @namespace    complete.misc
-// @version      2.0.26
+// @version      2.0.27
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Misc/complete.misc.v2.user.js
@@ -947,6 +947,7 @@
                 G_videoWidth = e.data.width;
                 G_videoHeight = e.data.height;
                 G_videoDuration = e.data.duration;
+                if (!G_embedCodeFrame) return;
                 G_embedCodeVideo = addEmbedCodeVideo(G_embedCodeFrame, G_funcToRun, function onLoadFunc() {
                     updateEmbedCodeTextColor();
                 }, function onErrorFunc() {
