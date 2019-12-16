d20ce7c344faac6d3607a68dec35af616a6bc6c0
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-12-17 00:12:57 +0200

    Add files via upload

---
+++ Services/YouTube.Video.Download.user.js
@@ -0,0 +1,140 @@
+// ==UserScript==
+// @name         YouTube.Video.Download
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @version      1.0.00
+// @description  Pure JavaScript version.
+// @author       Ægir
+// @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/YouTube.Video.Download.user.js
+// @homepageURL  https://github.com/Qetuoadgj/JavaScript/tree/master/Services
+// @grant        unsafeWindow
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @run-at       document-end
+// @noframes
+// @match        *://www.youtube.com/watch*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    // --------------------------------------------------
+    // https://stackoverflow.com/a/11384018
+    function openInNewTab(url) {
+        var win = window.open(url, '_blank');
+        win.focus();
+    }
+    // --------------------------------------------------
+    // Code by Jingsong Zhao: https://stackoverflow.com/a/45332232
+    function download(url, fileName) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'blob';
+        xhr.onprogress = function(event) {
+            if (event.lengthComputable) {
+                var percentComplete = (event.loaded / event.total)*100;
+                // yourShowProgressFunction(percentComplete);
+            }
+        };
+        xhr.onload = function(event) {
+            if (this.status == 200) {
+                _saveBlob(this.response, fileName);
+            }
+            else {
+                // yourErrorFunction()
+            }
+        };
+        xhr.onerror = function(event){
+            // yourErrorFunction()
+            openInNewTab(url);
+        };
+        xhr.send();
+    }
+    function _saveBlob(response, fileName) {
+        if(navigator.msSaveBlob){
+            // OK for IE10+
+            navigator.msSaveBlob(response, fileName);
+        }
+        else{
+            _html5Saver(response, fileName);
+        }
+    }
+    function _html5Saver(blob , fileName) {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        var url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        document.body.removeChild(a);
+    }
+    // --------------------------------------------------
+    //     function getQualityList() {
+    //         const t_table = JSON.parse(unsafeWindow.ytplayer.config.args.player_response).streamingData.adaptiveFormats;
+    //         let t_matched = {};
+    //         for (let t of Object.keys(t_table)) {
+    //             let table = t_table[t];
+    //             if (table.itag) {
+    //                 let itag = table.itag.toString();
+    //                 let format = table.mimeType;
+    //                 if (format && format.match('video/mp4')) { // if (t_itags[itag]) {
+    //                     t_matched[itag] = table;
+    //                 };
+    //             };
+    //         };
+    //         for (let t of Object.keys(t_matched)) {
+    //             let table = t_matched[t];
+    //             let quality = table.height + 'p', fps = table.fps + 'fps ', type = table.mimeType, url = table.url;
+    //             let type_2 = type.replace(/;.*/, '');
+    //             let ext = type_2.replace(/^.*\//, '')
+    //             let cmd = GM_registerMenuCommand(`${quality} ${fps} ${ext}`, function() {
+    //                 let fileName = document.title + ' - ' + quality + '.'+ ext;
+    //                 download(url, fileName)
+    //             }, '');
+    //         };
+    //     };
+    //     getQualityList();
+    // --------------------------------------------------
+    function getQualityList() {
+        const t_table = JSON.parse(unsafeWindow.ytplayer.config.args.player_response).streamingData.adaptiveFormats;
+        let t_matched = {};
+        for (let t of Object.keys(t_table)) {
+            let table = t_table[t];
+            if (table.itag) {
+                let itag = table.itag.toString();
+                let format = table.mimeType;
+                if (format && format.match('video/mp4')) {
+                    t_matched[itag] = table;
+                };
+            };
+        };
+        let sortingArray = [], sortedTable = {};
+        for (let t of Object.keys(t_matched)) {
+            let table = t_matched[t];
+            sortedTable[table.height] = sortedTable[table.height] ? sortedTable[table.height] : {};
+            sortedTable[table.height][table.fps] = sortedTable[table.height][table.fps] ? sortedTable[table.height][table.fps] : {};
+            sortedTable[table.height][table.fps][table.mimeType] = table;
+            if (!sortingArray.includes(table.height)) sortingArray.push(table.height)
+        };
+        sortingArray.sort(function(a, b){return a - b;});
+        for (let quality of sortingArray) {
+            let quality_table = sortedTable[quality.toString()];
+            for (let fps of Object.keys(quality_table)) {
+                let fps_table = quality_table[fps];
+                for (let mimeType of Object.keys(fps_table)) {
+                    let table = fps_table[mimeType];
+                    // console.log(quality, fps, mimeType, table);
+                    let type = mimeType.replace(/;.*/, '');
+                    let ext = type.replace(/^.*\//, '');
+                    let cmd = GM_registerMenuCommand(`${quality}p, ${fps}fps`, function() {
+                        let title = document.title;
+                        let fileName = `${title} - ${quality}p_${fps}fps.${ext}`;
+                        download(table.url, fileName)
+                    }, '');
+                };
+            };
+        };
+    };
+    getQualityList();
+})();
