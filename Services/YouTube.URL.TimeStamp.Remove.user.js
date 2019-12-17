7a89c9dfe45c919eb977f7a2d56be57e56fa3fa1
Qetuoadgj <a.sukhanovskiy@gmail.com>
2019-12-17 05:51:08 +0200

    Add files via upload

--- Services/YouTube.URL.TimeStamp.Remove.user.js
+++ Services/YouTube.URL.TimeStamp.Remove.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube.URL.TimeStamp.Remove
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
-// @version      1.0.02
+// @version      1.0.03
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/YouTube.URL.TimeStamp.Remove.user.js
@@ -9,18 +9,35 @@
 // @grant        none
 // @run-at       document-start
 // @noframes
-// @include     /^https?:\/\/www\.youtube\.com\/watch[?].*\bt=\d+s.*$/
+/// @include     /^https?:\/\/www\.youtube\.com\/watch[?].*\bt=\d+s.*$/
+// @match        *://www.youtube.com/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Your code here...
-    let b_Refresh = 1, s_Berfore = location.href, s_After = s_Berfore.replace(/^(.*)\bt=\d+s(.*)$/, '$1$2').replace(/&$/, '');
-    console.log('YouTube.URL.TimeStamp.Remove', s_Berfore, s_After);
-    if (b_Refresh) {
-        localStorage.alreadyRefreshed = true;
-        location.href = s_After;
+    function mainFunc() {
+        if (location.search.match(/^(.*)\bt=\d+s(.*)$/)) {
+            let s_Berfore = location.href, s_After = s_Berfore.replace(/^(.*)\bt=\d+s(.*)$/, '$1$2').replace(/&$/, '');
+            console.log('YouTube.URL.TimeStamp.Remove', s_Berfore, s_After);
+            localStorage.alreadyRefreshed = true;
+            location.href = s_After;
+        };
     }
-    else history.pushState(location.pathname, '', s_After);
+    // --------------------------------------------------
+    var videoElementSelector = [
+        '.html5-video-container > video', // [YouTube.com]
+    ].join(', ');
+    function handleNewElements(event) {
+        let element = event.target;
+        if (element.tagName == 'VIDEO') {
+            let myVideoElement = document.querySelectorAll(videoElementSelector)[0];
+            if (myVideoElement && myVideoElement == element) {
+                myVideoElement.addEventListener('loadedmetadata', mainFunc);
+                // document.removeEventListener('DOMNodeInserted', handleNewElements);
+            };
+        };
+    };
+    document.addEventListener('DOMNodeInserted', handleNewElements, false);
 })();
