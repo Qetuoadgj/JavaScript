7944498375cdb6e6db47211fca4a5dc2053eebc7
Qetuoadgj <a.sukhanovskiy@gmail.com>
2020-01-02 12:14:21 +0200

    Add files via upload

--- Services/YouTube.Audio.Boost.user.js
+++ Services/YouTube.Audio.Boost.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube.Audio.Boost
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
-// @version      1.0.11
+// @version      1.0.12
 // @description  Pure JavaScript version.
 // @author       Ægir
 // @downloadURL  https://github.com/Qetuoadgj/JavaScript/raw/master/Services/YouTube.Audio.Boost.user.js
@@ -77,11 +77,114 @@
         GM_unregisterMenuCommand(cmdOff);
         cmdOn = GM_registerMenuCommand(str_on_menu, function(){turnOn();}, '');
     };
+    // ================================================================================
+    function toHHMMSS(secs = 0) {
+        const sec_num = parseInt(secs, 10);
+        const hours = Math.floor(sec_num / 3600) % 24;
+        const minutes = Math.floor(sec_num / 60) % 60;
+        const seconds = sec_num % 60;
+        return [hours,minutes,seconds].map(v => v < 10 ? "0" + v : v).filter((v, i) => v !== "00" || i > 0).join(":");
+    }
+    // ================================================================================
+    function addMediaTextIndicator(media, fontSize) {
+        fontSize = fontSize || 72;
+        var mediaTextIndicator = document.createElement('div');
+        mediaTextIndicator.style.setProperty('color', 'yellow', 'important');
+        mediaTextIndicator.style['font-size'] = fontSize + 'px';
+        mediaTextIndicator.style.position = 'absolute';
+        mediaTextIndicator.style['z-index'] = 2147483647; // Always on TOP
+        mediaTextIndicator.style.top = '0px';
+        mediaTextIndicator.style.left = (fontSize/4) + 'px';
+        media.parentNode.insertBefore(mediaTextIndicator, media.nextSibling);
+        var volumeTextFade = function(fadeDelay) {
+            fadeDelay = fadeDelay || 2000;
+            var fadeDelaySeconds = Math.floor(fadeDelay/1000);
+            function textFadeStart(show) {
+                var transition = show ? '' : ('opacity '+fadeDelaySeconds+'s');
+                mediaTextIndicator.style.opacity = show ? 1 : 0;
+                mediaTextIndicator.style.transition = transition;
+                mediaTextIndicator.style['-webkit-transition'] = transition; // Safari
+            }
+            textFadeStart(true);
+            setTimeout(textFadeStart, fadeDelaySeconds*1000);
+        };
+        var setVolumeText = function() {
+            volumeTextFade(2000);
+            mediaTextIndicator.textContent = Math.round(media.volume * 100) > 0 ? Math.round(media.volume * 100) : 'Выкл.';
+        };
+        var setTimeText = function() {
+            volumeTextFade(2000);
+            var duration = media.duration;
+            var currentTime = media.currentTime;
+            mediaTextIndicator.textContent = (toHHMMSS(currentTime) + "/" + toHHMMSS(duration));
+        };
+        var addEventHandlers = function() {
+            if (media.addEventListener) {
+                media.addEventListener("volumechange", setVolumeText, false); // IE9, Chrome, Safari, Opera
+                media.addEventListener("seeking", setTimeText, false); // IE9, Chrome, Safari, Opera
+            }
+            else {
+                media.attachEvent("onvolumechange", setVolumeText); // IE 6/7/8
+                media.attachEvent("onseeking", setTimeText); // IE 6/7/8
+            }
+        };
+        setTimeout(addEventHandlers, 10);
+        return mediaTextIndicator;
+    };
+    // ================================================================================
+    function mediaMouseControls(eventCatcher, media, step) {
+        step = (step === 0) ? 0 : (step || 1);
+        var mouseWheelAudioHandler = function(e) {
+            if (step !== 0) {
+                // cross-browser wheel delta
+                e = window.event || e; // old IE support
+                var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+                var amount = parseInt(delta*step), volume = parseInt(media.volume*100);
+                var value = amount > 0 ? Math.floor((volume+amount)/step)*step : Math.ceil((volume+amount)/step)*step;
+                media.volume = Math.max(0, Math.min(100, value)) / 100;
+            }
+            e.preventDefault();
+        };
+        var mouseWheelTimeHandler = function(e) {
+            if (step !== 0) {
+                // cross-browser wheel delta
+                e = window.event || e; // old IE support
+                var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+                var amount = parseInt(delta*step);
+                var mediaState = media.paused ? 0 : 1;
+                setTimeout(function() {
+                    if (delta < 0) {
+                        media.pause(); media.currentTime = parseInt(media.currentTime) - 5; if (mediaState == 1) media.play();
+                    }
+                    else if (delta > 0) {
+                        media.pause(); media.currentTime = parseInt(media.currentTime) + 5; if (mediaState == 1) media.play();
+                    }
+                }, 10);
+            }
+            e.preventDefault();
+        };
+        let startHandlerEvent = function(e) {
+            var shiftDown = !!window.event.shiftKey;
+            if (shiftDown) mouseWheelTimeHandler(e);
+            else mouseWheelAudioHandler(e);
+        }
+        if (media.addEventListener) {
+            eventCatcher.addEventListener("mousewheel", startHandlerEvent, false); // IE9, Chrome, Safari, Opera
+            eventCatcher.addEventListener("DOMMouseScroll", startHandlerEvent, false); // Firefox
+        }
+        else {
+            eventCatcher.attachEvent("onmousewheel", startHandlerEvent); // IE 6/7/8
+        }
+        var mediaTextIndicator = addMediaTextIndicator(media, 56/2);
+        mediaTextIndicator.style.top = '5px';
+    };
+    // ================================================================================
     // --------------------------------------------------
     let handleNewElements; function initFunction() {
         let myVideoElement = document.querySelectorAll(videoElementSelector)[0]; // 1st match
         if (myVideoElement) {
             document.removeEventListener('DOMNodeInserted', handleNewElements);
+            if (location.host == 'rezka.ag') mediaMouseControls(myVideoElement, myVideoElement, 1);
             G_gainNode = connectBoost(myVideoElement);
             if (G_gainNode) {
                 GM_registerMenuCommand(str_title_menu, function(){callPrompt(G_gainNode);}, '');
